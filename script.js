e77af3fe4fdf1d9e17ae259e9f08922bf28a62a6
cmanor <101116379+cmanor@users.noreply.github.com>
2022-04-08 14:01:42 -0400

    working, switching to template literal

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
     const apiKey= "856cf893bf4748da5e5e119d50885a54";
-    //gets weather from
-    const fetchWeather = function (city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-        .then((response) => {
-          if (!response.ok) {
-            alert("No weather found.");
-            throw new Error("No weather found.");
-          }
-          return response.json();
-        })
-        .then((data) => displayWeather(data));
-    };
-    const displayWeather = function (data) {
-      const {name} = data;
-      const {icon, description} = data.weather[0];
-      const {temp, humidity} = data.main;
-      const {speed} = data.wind;
-      document.querySelector(".city").innerText = name;
-      document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-      document.querySelector(".weather").classList.remove("loading");
-    //   document.body.style.backgroundImage =
-    //     "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    };
-    const search = function () {
-      fetchWeather(document.querySelector(".search-bar").value);
-    };
+    //getting zipcode data b/c I need to send lon and lat to another api
+    const zipData = function (zip) {
+        fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${apiKey}`)
+          .then((response) => 
+              response.json())
+              //sends data to next api request
+            .then((data) => lonlat(data))
+      };
+//sends lon and lat to 2nd API that has weather data
+      const lonlat = function (deez) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${deez.lat}&lon=${deez.lon}&units=imperial&appid=${apiKey}`)
+          .then((response) => 
+              response.json())
+              //send data into the display weather function
+              .then(data => displayWeather(data))
+      };
+//function for displaying weather data
+      const displayWeather = function (data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity, temp_min, temp_max, feels_like} = data.main;
+        const {speed} = data.wind;
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText =
+          "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+          "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+      };
+      const search = function () {
+        zipData(document.querySelector(".search-bar").value);
+      };
 
-  
   document.querySelector(".search button").addEventListener("click", function () {
     search();
   });
@@ -46,4 +48,4 @@
       }
     });
 
-    fetchWeather("charlotte");
+    zipData("28205");
